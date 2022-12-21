0d2672a395b2a59ad570ff493342947d070bd513
Nana Reinikainen <nana.reinikainen@helsinki.fi>
2022-12-21 23:32:36 +0200

    add simple cache

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 import requests
 from flask import Flask, render_template
+from flask_caching import Cache
 
 app = Flask(__name__)
+cache = Cache()
+app.config['CACHE_TYPE'] = 'simple'
+cache.init_app(app)
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
 @app.route("/projects")
+@cache.cached(timeout=60) # 43200 = 12 tuntia
 def get_repos():
     api_url = "https://api.github.com/users/Na-na13/repos"
     response = requests.get(api_url).json()
